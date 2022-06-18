b453682b4d1b7c6e06e1d1132674e5117c9937a1
David Fueltala <davidfueltala90@hotmail.com>
2022-06-17 20:16:55 -0500

    Taller 12 LookUp

--- src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.uce.edu.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
 import com.uce.edu.demo.banco.modelo.ProfesorGeneral;
@@ -20,11 +21,27 @@ public class MatriculaServiceImpl implements IMatriculaService{
 	@Autowired
 	private ProfesorMateria profesorMateria;
 	
+	@Lookup
+	public ProfesorGeneral obtenerProfesorG() {
+		return null;
+	}
+	
+	@Lookup
+	public ProfesorMateria obtenerProfesorM() {
+		ProfesorMateria profeM = new ProfesorMateria();
+		profeM.setNombre("Tapia");
+		profeM.setApellido("Jose");
+		return profeM;
+	}
+	
 	@Override
 	public void ingresarMatricula(Matricula ma) {
 		// TODO Auto-generated method stub
 		System.out.println("DI desde Service SINGLETON" + this.profesorGeneral);
 		System.out.println("DI desde Service PROTOTYPE" + this.profesorMateria);
+		
+		System.out.println("DI un ejemplo General: " + this.obtenerProfesorG());
+		System.out.println("DI un ejemplo Materia: " + this.obtenerProfesorM());
 		this.iMatriculaRepository.insertarMatricula(ma);
 		
 	}
